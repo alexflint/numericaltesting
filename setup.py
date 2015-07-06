afa3605e512a5cf462a6bdb313dffd76cf3a2dff
Alex Flint <alex.flint@gmail.com>
2015-07-05 20:23:12 -0700

    Release version 0.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(name='numericaltesting',
       description='Tools for unittesting with arrays and numerical algorithms',
-      version='0.1',
+      version='0.11',
       author='Alex Flint',
       author_email='alex.flint@gmail.com',
       url='https://github.com/alexflint/numericaltesting',
